ab8eae5fc0c96f26a0c2dca24dd408647d0ebf62
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 02:26:28 -0400

    chore(iac): created subnet

---
+++ src/iac/subnet.tf
@@ -0,0 +1,6 @@
+resource "aws_subnet" "this" {
+  vpc_id            = aws_vpc.this.id
+  availability_zone = var.aws_availability_zone
+  cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, 4, 1)
+}
+
