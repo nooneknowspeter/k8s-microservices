811147f49c83f0e1675af92bbbec03030863b651
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 02:40:57 -0400

    chore(iac): created bash script to install minikube on x86 arc

---
+++ src/iac/deployment/installation/minikube.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "\n installing minikube \n"
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
