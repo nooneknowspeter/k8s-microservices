38e8f0ec40309de9785ec230efa1fa6dbbad2a8f
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 02:36:28 -0400

    chore(iac): created output to display the ec2 instance's private-ip-address

--- src/iac/output.tf
+++ src/iac/output.tf
@@ -8,3 +8,8 @@ output "instance_public_ip" {
   description = "AWS Instance Public IP Address"
 }
 
+output "instance_private_ip" {
+  value       = aws_instance.this.private_ip
+  description = "AWS Instance Private IP Address"
+}
+
