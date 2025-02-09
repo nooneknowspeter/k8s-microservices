c806afdb338da6a4aedfaf1a477cd3dced72d2cd
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 02:31:33 -0400

    chore(iac): added bash script to install dependencies and deploy applications on ec2 instance start up

--- src/iac/ec2.tf
+++ src/iac/ec2.tf
@@ -2,10 +2,16 @@ resource "aws_instance" "this" {
   ami                         = var.aws_ami
   instance_type               = var.aws_instance_type
   associate_public_ip_address = true
+
   tags = {
     Name = var.aws_instance_name
   }
+
   vpc_security_group_ids = [aws_security_group.this.id]
+
   subnet_id = aws_subnet.this.id
+
+  # initial setup of VM; install all dependencies 
+  user_data = file("./deployment/main.sh")
 }
 
