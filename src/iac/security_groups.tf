ddeab39fc71138773d2aaf9183aa47fa3cf42bfc
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 02:27:23 -0400

    chore(iac): created ingress for all ports and protocols

--- src/iac/security_groups.tf
+++ src/iac/security_groups.tf
@@ -2,4 +2,10 @@ resource "aws_security_group" "this" {
   name        = var.aws_security_group_name
   description = "Allow All Traffic"
   vpc_id      = aws_vpc.this.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
