dff81b2f81eb01705f3ec6d25973f37235f2fbe9
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-09 02:29:38 -0400

    chore(iac): mapped varible to tags of ec2 instance

--- src/iac/ec2.tf
+++ src/iac/ec2.tf
@@ -2,5 +2,8 @@ resource "aws_instance" "this" {
   ami                         = var.aws_ami
   instance_type               = var.aws_instance_type
   associate_public_ip_address = true
+  tags = {
+    Name = var.aws_instance_name
+  }
 }
 
