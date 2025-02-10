3e6659fbb99e2cce08d28d3e30a0526f438a666e
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-10 03:06:36 -0400

    refactor(iac): renamed resource for ec2 instance

--- src/iac/7-ec2.tf
+++ src/iac/7-ec2.tf
@@ -2,12 +2,13 @@ resource "aws_instance" "this" {
   ami                         = var.aws_ami
   instance_type               = var.aws_instance_type
   associate_public_ip_address = true
+  availability_zone           = var.aws_availability_zone
 
   tags = {
     Name = var.aws_instance_name
   }
 
-  vpc_security_group_ids = [aws_security_group.this.id]
+  vpc_security_group_ids = ["${aws_security_group.this.id}"]
 
   subnet_id = aws_subnet.this.id
 
