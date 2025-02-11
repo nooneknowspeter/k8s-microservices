8626def63837fffdf77a7ebf79c5e1883683e834
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 05:04:02 -0400

    refactor(iac): explicitly added localhost to URI

--- src/iac/deployment/configurations/reverse_proxy.sh
+++ src/iac/deployment/configurations/reverse_proxy.sh
@@ -2,8 +2,8 @@
 
 echo -e "\n reverse proxy with apache httpd \n"
 
-PROXY_TO="$(kubectl get ingress | grep -o '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')"
+# PROXY_TO="$(kubectl get ingress | grep -o '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')"
 
-sudo bash -c "echo 'LoadModule proxy_module modules/mod_proxy.so <VirtualHost *:8080> ProxyPass / http://$PROXY_TO/ ProxyPassReverse / http://$PROXY_TO/ ' >> /etc/httpd/conf/httpd.conf"
+sudo bash -c "echo 'LoadModule proxy_module modules/mod_proxy.so <VirtualHost * 8080> ProxyPass / http://localhost/ ProxyPassReverse / http://localhost/ ' >> /etc/httpd/conf/httpd.conf"
 
 sudo systemctl restart httpd
