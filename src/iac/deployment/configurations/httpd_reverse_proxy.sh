6e8382eade3c34823ed2c283589d5b6608d461ba
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 01:56:46 -0400

    refactor(iac): renamed script to reverse_proxy.sh

--- src/iac/deployment/configurations/httpd_reverse_proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\n reverse proxy with apache httpd \n"
-
-PROXY_TO="$(kubectl get ingress | grep -o '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')"
-
-sudo bash -c "echo 'LoadModule proxy_module modules/mod_proxy.so <VirtualHost *:80> ProxyPass / http://$PROXY_TO/ ProxyPassReverse / http://$PROXY_TO/ ' >> /etc/httpd/conf/httpd.conf"
-
-sudo system restart httpd
