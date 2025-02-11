9f2932b0ce226f14649829f68898acdb8213e437
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 04:59:52 -0400

    refactor(iac): allowed all traffic through all ports

--- src/iac/6-security_group.tf
+++ src/iac/6-security_group.tf
@@ -2,26 +2,16 @@ resource "aws_security_group" "this" {
   vpc_id = aws_vpc.this.id
 
   ingress {
-    description = "HTTP Access To Cluster"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Enable SSH Access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
