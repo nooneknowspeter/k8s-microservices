2753db75d4ae397518cc4143c451fb8f3da11ac4
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 06:33:50 -0400

    refactor(iac): re-added security groups for ports 22 and 80

--- src/iac/6-security_group.tf
+++ src/iac/6-security_group.tf
@@ -2,9 +2,16 @@ resource "aws_security_group" "this" {
   vpc_id = aws_vpc.this.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
