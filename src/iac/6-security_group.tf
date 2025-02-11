a1076cb15e8a2c445a2a5a10d17f1f0a68bd4da3
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 01:54:46 -0400

    refactor(iac): redirect port 8080 to external

--- src/iac/6-security_group.tf
+++ src/iac/6-security_group.tf
@@ -9,14 +9,17 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
   ingress {
     description = "Enable SSH Access"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
