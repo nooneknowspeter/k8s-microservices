bcd6b6139c6e5911b9a1d337c6af5f44edb3c4ba
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 22:16:56 -0400

    refactor(iac): updated user_data property on aws_instance

--- src/iac/7-ec2.tf
+++ src/iac/7-ec2.tf
@@ -12,9 +12,10 @@ resource "aws_instance" "this" {
 
   subnet_id = aws_subnet.this.id
 
+  key_name = aws_key_pair.this.key_name
+
   # initial setup of VM; install all dependencies 
-  user_data                   = "./deployment/user_data.sh"
+  user_data                   = file(var.aws_user_data)
   user_data_replace_on_change = true
-  # user_data_base64            = "./deployment/user_data.sh"
 }
 
