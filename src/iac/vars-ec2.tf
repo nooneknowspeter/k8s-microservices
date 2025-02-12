5bd3b76ce6b2c37db0ac85a485a23375ea2fd089
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 22:21:29 -0400

    chore(iac): added user_data variable

--- src/iac/vars-ec2.tf
+++ src/iac/vars-ec2.tf
@@ -15,3 +15,9 @@ variable "aws_ami" {
   description = "AWS AMI | Default = Amazon Linux 2023 AMI (Free Tier)"
   default     = "ami-088b41ffb0933423f"
 }
+
+variable "aws_user_data" {
+  type        = string
+  description = "AWS EC2 Commands To Run On Instance Launch"
+  default     = "echo hello"
+}
