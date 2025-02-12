e55983566d9dda56692369ccc211efa3b2fdb12f
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 22:16:16 -0400

    refactor(iac): updated security groups

--- src/iac/6-security_group.tf
+++ src/iac/6-security_group.tf
@@ -1,17 +1,26 @@
 resource "aws_security_group" "this" {
   vpc_id = aws_vpc.this.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description = "http"
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   description = "ssh"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
