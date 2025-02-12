0fa21d112863f10b3e80cf67aed627944f225096
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-12 12:17:59 -0400

    chore(iac): added local exec to parse and store generated private key from tls to a .pem file with chmod 400 applied

--- src/iac/8-key_pair.tf
+++ src/iac/8-key_pair.tf
@@ -6,5 +6,12 @@ resource "tls_private_key" "this" {
 resource "aws_key_pair" "this" {
   key_name   = var.aws_key_name
   public_key = tls_private_key.this.public_key_openssh
+
+  provisioner "local-exec" { # Generate aws_ssh.pem" in current directory
+    command = <<-EOT
+      echo '${tls_private_key.this.private_key_pem}' > ./aws_ssh.pem
+      chmod 400 ./aws_ssh.pem
+    EOT
+  }
 }
 
