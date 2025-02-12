acc0acabc6976a40e2092fe8331fad0d3b635d9b
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-12 12:20:53 -0400

    chore(iac): added tls provider

--- src/iac/providers.tf
+++ src/iac/providers.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
   }
 }
 
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
+}
+
+provider "tls" {
+  # Configuration options
 }
