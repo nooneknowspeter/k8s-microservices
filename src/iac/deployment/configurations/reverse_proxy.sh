46b0bd592545b982ae4ca0e028c06105804755cd
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-11 22:18:04 -0400

    refactor(iac): updated script for httpd reverse proxy

--- src/iac/deployment/configurations/reverse_proxy.sh
+++ src/iac/deployment/configurations/reverse_proxy.sh
@@ -4,6 +4,9 @@ echo -e "\n reverse proxy with apache httpd \n"
 
 PROXY_TO="localhost"
 
-sudo bash -c "echo 'LoadModule proxy_module modules/mod_proxy.so <VirtualHost localhost> ProxyPass / http://$PROXY_TO/ ProxyPassReverse / http://$PROXY_TO/ ' >> /etc/httpd/conf/httpd.conf"
+sudo /bin/su -c "echo '
+ProxyPass / http://$PROXY_TO/ 
+ProxyPassReverse / http://$PROXY_TO/
+' >> /etc/httpd/conf/httpd.conf"
 
 sudo systemctl restart httpd
