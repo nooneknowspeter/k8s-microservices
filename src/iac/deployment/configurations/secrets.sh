820b98ce06b251fba1eb3c080327937aca733c64
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-02-15 23:13:16 -0400

    refactor(iac): updated secrets scripts

--- src/iac/deployment/configurations/secrets.sh
+++ src/iac/deployment/configurations/secrets.sh
@@ -10,40 +10,37 @@ NC='\033[0m' # no color
 
 # create secret for database access
 GET_PASSWORD() {
-  echo -e "\n Enter a password (Length > $DESIREDPASSWORDLENGTH, UPPERCASE, lowercase): "
+	echo -e "\n Enter a password (Length > $DESIREDPASSWORDLENGTH, UPPERCASE, lowercase): "
 
-  read -r PASSWORD
+	read -r PASSWORD
 
-  if [[ ${#PASSWORD} -le DESIREDPASSWORDLENGTH ]]; then
-    echo -e "${RED}\n Password is less than DESIREDPASSWORDLENGTH characters${NC}"
+	if [[ ${#PASSWORD} -le DESIREDPASSWORDLENGTH ]]; then
+		echo -e "${RED}\n Password is less than DESIREDPASSWORDLENGTH characters${NC}"
 
-    GET_PASSWORD
-  elif [[ ! $PASSWORD =~ [^a-zA-Z0-9] ]]; then
-    echo -e "${RED}\n Password must contain UPPERCASE, lowercase letters, base-10 digits and/or non-alphanumeric symbols${NC}"
+		GET_PASSWORD
+	elif [[ ! $PASSWORD =~ [^a-zA-Z0-9] ]]; then
+		echo -e "${RED}\n Password must contain UPPERCASE, lowercase letters, base-10 digits and/or non-alphanumeric symbols${NC}"
 
-    GET_PASSWORD
-  fi
+		GET_PASSWORD
+	fi
 }
 
 GET_PASSWORD
 
 # delete any secrets file with the same name
-if kubectl delete secrets $NAME; then
-  echo -e "\n Secret Successfully Deleted"
+if kubectl delete secrets "$NAME"; then
+	echo -e "\n Secret Successfully Deleted"
 else
-  echo -e "${RED}\n Secret Does Not Exist${NC}"
+	echo -e "${RED}\n Secret Does Not Exist${NC}"
 fi
 
 # create secret
-if kubectl create secret generic $NAME --from-literal=SA_PASSWORD="${PASSWORD}"; then
-  echo -e "\n Secret Successfully Created \n"
+if kubectl create secret generic "$NAME" --from-literal=SA_PASSWORD="$PASSWORD"; then
+	echo -e "\n Secret Successfully Created \n"
 
-  echo -e "\n Name of secret: $NAME \n"
+	echo -e "\n Name of secret: $NAME \n"
 
-  kubectl get secrets
+	kubectl get secrets
 else
-  echo -e "${RED}\n Failed \n${NC}"
+	echo -e "${RED}\n Failed \n${NC}"
 fi
-
-# restart database deployment
-kubectl rollout restart deployment mssql-deployment
