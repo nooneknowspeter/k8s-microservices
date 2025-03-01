0f61e76f24f3c21108aeb3009610599b81601af5
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-03-01 04:08:10 -0400

    chore(iac): added display messages to show which ports are being opened in console

--- src/iac/deployment/configurations/port-forwarding.sh
+++ src/iac/deployment/configurations/port-forwarding.sh
@@ -3,16 +3,20 @@
 # forward ports to services in detached state for development purposes
 
 # node ports
+echo -e "\n --> forwarding node port"
 kubectl port-forward service/platform-node-port-service 8080:8080 &
 
 # databases
+echo -e "\n --> forwarding sql server load balancer"
 kubectl port-forward service/mssql-loadbalancer 1433:1433 &
 
 # message queues
+echo -e "\n --> forwarding rabbitmq load balancer"
 kubectl port-forward service/rabbitmq-loadbalancer 15672:15672 &
 kubectl port-forward service/rabbitmq-loadbalancer 5672:5672 &
 
 # monitoring
+echo -e "\n --> forwarding open telemetry and lgtm load balancer"
 kubectl port-forward service/lgtm-loadbalancer 3000:3000 &
 kubectl port-forward service/lgtm-loadbalancer 4317:4317 &
 kubectl port-forward service/lgtm-loadbalancer 4318:4318 &
